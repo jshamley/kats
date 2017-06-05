73a8b54805759121f87793c6b18f0c9349942a29
Jay Vickers <jvickers@healthgrades.com>
2017-06-05 15:24:28 -0600

    change styling

--- src/App.js
+++ src/App.js
@@ -7,16 +7,20 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <h1>KATS</h1>
-          <h3>We love 'em because they hate us</h3>
+          <h3>Everything you need for your Kat book report in one place.</h3>
         </div>
         <div className="site-content">
           <img src={"/assets/Garfield.png"} alt="Garfield" className="intro-img" />
           <div className="intro-text">
-            <p>We’re excited you’re here to celebrate our friend the Kat!</p>
-            <p>Our vision is to be the home for all of your Kat resources for fans, owners, and those that are curious…like Kats.</p>
+            {/*<p>Here you can find Kat information, facts, pictures, and more!</p>*/}
+            {/*<p>We’re excited you’re here to celebrate our friend the Kat!</p>*/}
+            {/*<p>Our vision is to be the home for all of your Kat resources for fans, owners, and those that are curious…like Kats.</p>*/}
 
-            <h4>Come back later today for updates!</h4>
+            
           </div>
+        </div>
+        <div className="site-footer">
+          <h4>Come back later today for updates!</h4>
         </div>        
       </div>
     );
