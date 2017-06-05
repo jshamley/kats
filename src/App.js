c43768e541ab4c947e07baea745093c1704de3f5
Jeff Shamley <jshamley@comcast.net>
2017-06-05 10:28:43 -0600

    text and css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,16 +6,16 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>We Love KATs</h2>
+          <h1>KATS</h1>
+          <h3>We love 'em</h3>
         </div>
         <div className="site-content">
           <img src={"/assets/Garfield.png"} alt="Garfield" className="intro-img" />
           <div className="intro-text">
-            <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur consequuntur, dignissimos pariatur mollitia aliquid maiores sit dolores dolore magnam nostrum adipisci cum quis enim animi obcaecati. Quae iusto, fugiat cumque.</div>
-            <div>Veniam quos assumenda eligendi atque quo molestias, consequatur nihil cum ea sequi delectus ab obcaecati fugit expedita quas, aliquid deleniti nulla ducimus neque quasi facilis numquam excepturi. Sit, assumenda, repudiandae.</div>
-            <div>Voluptatibus sunt explicabo possimus recusandae quasi velit dolorem culpa quibusdam, ipsum labore ab molestias nihil suscipit nisi incidunt, unde iusto laborum laboriosam, tempore fugit minus. Itaque vero doloremque, asperiores tenetur?</div>
-            <div>Ex aperiam accusamus vitae blanditiis nesciunt a tempore perspiciatis, amet mollitia, quia, eius possimus voluptatem numquam tempora reprehenderit saepe labore autem delectus explicabo! Aspernatur nobis, molestiae rem debitis impedit nisi.</div>
+            <p>We’re excited you’re here to celebrate our friend the Kat!</p>
+            <p>Our vision is to be the home for all of your Kat resources for fans, owners, and those that are curious…like Kats.</p>
+
+            <button>Come back later today for updates!</button>
           </div>
         </div>        
       </div>
