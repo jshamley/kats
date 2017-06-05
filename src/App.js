8229646c95c6b02f0cc5b2310477faae11b63af9
Jeff Shamley <jshamley@comcast.net>
2017-06-05 11:03:04 -0600

    app header

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <h1>KATS</h1>
-          <h3>We love 'em</h3>
+          <h3>We love 'em because they hate us</h3>
         </div>
         <div className="site-content">
           <img src={"/assets/Garfield.png"} alt="Garfield" className="intro-img" />
@@ -15,7 +15,7 @@ class App extends Component {
             <p>We’re excited you’re here to celebrate our friend the Kat!</p>
             <p>Our vision is to be the home for all of your Kat resources for fans, owners, and those that are curious…like Kats.</p>
 
-            <button>Come back later today for updates!</button>
+            <h4>Come back later today for updates!</h4>
           </div>
         </div>        
       </div>
