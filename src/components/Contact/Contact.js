12f80aaf581fe43019b262b6768147152ca831fc
Jay Vickers <jvickers@healthgrades.com>
2017-06-06 11:52:49 -0600

    add contact us page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
+import './contact.css';
 
 class Contact extends Component {
   render() {
     return (
-      <div>
-        <h2 className="page-title">Contact</h2>
+      <div className='contact-page'>
+        <h2 className="page-title">Contact Information</h2>
         <div className="text-left">
-          
+          We have expert Kat enthusiasts on staff waiting to help with any additional questions about Kats. Contact us electronically or email.
+          <div className='email-block'>
+            Contact us electronically at <a href='mailto:rcaddell@healthgrades.com'>Contact Us</a>
+            </div>
+            <div className='address-block'>
+              Contact us by snail mail:
+              <div className='address'>
+              <div>Katz</div>
+              <div>999 18th Avenue Suite 500</div>
+              <div>Denver, CO 80202</div>
+              </div>
+            </div>
         </div>
       </div>
     );
