0f738cc5dd942d13672df818836462001cc55aa9
Jay Vickers <jvickers@healthgrades.com>
2017-06-06 11:43:10 -0600

    add stuff to anatomy

--- src/components/Anatomy/Anatomy.js
+++ src/components/Anatomy/Anatomy.js
@@ -6,7 +6,15 @@ class Anatomy extends Component {
       <div>
         <h2 className="page-title">Anatomy &amp; Biology</h2>
         <div className="text-left">
-          
+          <h3> General Information </h3>
+          <ul>
+            <li>An adult domestic cat's average height in the range of 8 to 10 inches or 20 to 25 cm. </li>
+            <li>Female domestic short hair cats weigh between 6 to 10 pounds or 2.7 to 4.5 KG.</li>
+            <li>Average male domestic short hair cats can weigh anywhere from 10 to 12 pounds or 4.5 to 5.5 KG.</li>
+            <li>Cats' tails range from long, thin, and whip-like to short, thick, and plume-like. The Siamese' tail ends somewhere over the rainbow, while the Manx' tail stops before it begins. The Japanese Bobtail's tail is curled, corkscrewed, and looks like a pom-pom. Cats use their tails as balancing poles and to keep their noses warm when they sleep.</li>
+            <li>Breathes using typical mamillian lung structure.</li>
+            <li>Average lifespan of 12-15 years in captivity, 4-5 years in the wild.</li>
+            </ul>
         </div>
       </div>
     );
